6591ae2bb01d394ee9dcdf6b7786557b43c8822d
SyedFarzeenAhmad <syedfarzeen18@gmail.com>
2025-04-14 23:53:33 +0530

    Basic Template

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    myDateTime(); // optional initial call
+    setInterval(myDateTime, 1000);
+};
+
+
+//Date and Time
+function myDateTime()
+{
+    const d = new Date();
+    document.querySelector(".date").innerHTML = "Date: " + d.toDateString();
+    document.querySelector(".time").innerHTML = "Time: " + d.toLocaleTimeString();
+}
+
+setInterval(myDateTime, 1000);
